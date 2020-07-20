52d2a583d339c9cbb515a9dd22e19f6db9c10374
Trevor Babcock <trevor.d.babcock@gmail.com>
2020-07-20 12:23:16 -0700

    Added seed for randomness and added mountains
    Mountains have a lot of room for improvement.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## Hunter
 This is a simple project to explore the idea of simulating a hunter trying to survive in his/her environment, and the development of all the systems necessary to accomplish that.
+
+For more info, see blog entries in the blog directory.
--- bin/generate_map.rb
+++ bin/generate_map.rb
@@ -1,11 +1,20 @@
 #!/usr/bin/env ruby
 
+SEED = 1235
+
 WATER = " ~"
 LAND = " G"
+MOUNTAIN = " ^"
 
-LAND_ADJ_COEF_BASE = 10
+MAX = 100
+
+LAND_ADJ_COEF_BASE = 8
 LAND_ADJ_COEF_ADD = 26
 LAND_ADJ_COEF_DIAG_ADD = 18
+
+MOUNTAIN_ADJ_COEF_BASE = 2
+MOUNTAIN_ADJ_COEF_ADD = 25
+MOUNTAIN_ADJ_COEF_DIAG_ADD = 5
 
 def generate_map(height, width)
   map = []
@@ -17,6 +26,7 @@ def generate_map(height, width)
     end
   end
   map = generate_land(map)
+  map = generate_mountains(map)
 
   return map
 end
@@ -25,29 +35,81 @@ def generate_land(map)
   map.each_with_index do |row,y|
     row.each_with_index do |cell,x|
       likelihood = LAND_ADJ_COEF_BASE
-      likelihood = likelihood + LAND_ADJ_COEF_ADD if get_cell_or_nil(map, x+1, y) == LAND # up
-      likelihood = likelihood + LAND_ADJ_COEF_ADD if get_cell_or_nil(map, x-1, y) == LAND # down
-      likelihood = likelihood + LAND_ADJ_COEF_ADD if get_cell_or_nil(map, x, y-1) == LAND # left
-      likelihood = likelihood + LAND_ADJ_COEF_ADD if get_cell_or_nil(map, x, y+1) == LAND # right
-      likelihood = likelihood + LAND_ADJ_COEF_DIAG_ADD if get_cell_or_nil(map, x-1, y-1) == LAND # upleft
-      likelihood = likelihood + LAND_ADJ_COEF_DIAG_ADD if get_cell_or_nil(map, x-1, y+1) == LAND # downleft
-      likelihood = likelihood + LAND_ADJ_COEF_DIAG_ADD if get_cell_or_nil(map, x+1, y-1) == LAND # upright
-      likelihood = likelihood + LAND_ADJ_COEF_DIAG_ADD if get_cell_or_nil(map, x+1, y+1) == LAND # downright
+      likelihood = likelihood + LAND_ADJ_COEF_ADD if get_north(map, x, y) == LAND
+      likelihood = likelihood + LAND_ADJ_COEF_ADD if get_south(map, x, y) == LAND
+      likelihood = likelihood + LAND_ADJ_COEF_ADD if get_west(map, x, y) == LAND
+      likelihood = likelihood + LAND_ADJ_COEF_ADD if get_east(map, x, y) == LAND
+      likelihood = likelihood + LAND_ADJ_COEF_DIAG_ADD if get_northeast(map, x, y) == LAND
+      likelihood = likelihood + LAND_ADJ_COEF_DIAG_ADD if get_southeast(map, x, y) == LAND
+      likelihood = likelihood + LAND_ADJ_COEF_DIAG_ADD if get_southwest(map, x, y) == LAND
+      likelihood = likelihood + LAND_ADJ_COEF_DIAG_ADD if get_northwest(map, x, y) == LAND
 
-      if rand(100) < likelihood
+      if rand(MAX) < likelihood
         map[x][y] = LAND
       end
     end
   end
 end
 
+def generate_mountains(map)
+  map.each_with_index do |row,y|
+    row.each_with_index do |cell,x|
+      if map[x][y] != WATER
+        likelihood = MOUNTAIN_ADJ_COEF_BASE
+        likelihood = likelihood + MOUNTAIN_ADJ_COEF_ADD if get_north(map, x, y) == MOUNTAIN
+        likelihood = likelihood + MOUNTAIN_ADJ_COEF_ADD if get_south(map, x, y) == MOUNTAIN
+        likelihood = likelihood + MOUNTAIN_ADJ_COEF_ADD if get_east(map, x, y) == MOUNTAIN
+        likelihood = likelihood + MOUNTAIN_ADJ_COEF_ADD if get_west(map, x, y) == MOUNTAIN
+        likelihood = likelihood + MOUNTAIN_ADJ_COEF_DIAG_ADD if get_northeast(map, x, y) == MOUNTAIN
+        likelihood = likelihood + MOUNTAIN_ADJ_COEF_DIAG_ADD if get_southeast(map, x, y) == MOUNTAIN
+        likelihood = likelihood + MOUNTAIN_ADJ_COEF_DIAG_ADD if get_southwest(map, x, y) == MOUNTAIN
+        likelihood = likelihood + MOUNTAIN_ADJ_COEF_DIAG_ADD if get_northwest(map, x, y) == MOUNTAIN
+
+        if rand(MAX) < likelihood
+          map[x][y] = MOUNTAIN
+        end
+      end
+    end
+  end
+end
+
 def get_cell_or_nil(map, x, y)
-  tmp = map[x]
   if(map[x])
-    tmp2 = map[x][y]
     return map[x][y]
   end
   return nil
+end
+
+def get_north(map, x, y)
+  get_cell_or_nil(map, x, y+1)
+end
+
+def get_northeast(map, x, y)
+  get_cell_or_nil(map, x+1, y+1)
+end
+
+def get_east(map, x, y)
+  get_cell_or_nil(map, x+1, y)
+end
+
+def get_southeast(map, x, y)
+  get_cell_or_nil(map, x+1, y-1)
+end
+
+def get_south(map, x, y)
+  get_cell_or_nil(map, x, y-1)
+end
+
+def get_southwest(map, x, y)
+  get_cell_or_nil(map, x-1, y-1)
+end
+
+def get_west(map, x, y)
+  get_cell_or_nil(map, x-1, y)
+end
+
+def get_northwest(map, x, y)
+  get_cell_or_nil(map, x-1, y+1)
 end
 
 def print_map(map)
@@ -59,5 +121,6 @@ def print_map(map)
   end
 end
 
+srand(SEED)
 map = generate_map(64, 64)
 print_map(map)
