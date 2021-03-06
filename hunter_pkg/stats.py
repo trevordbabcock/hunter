464c5b30b40f87fe6e065f62600eec5520c799b9
Trevor Babcock <trevor.d.babcock@gmail.com>
2021-03-06 11:30:01 -0800

    Converted stats.json to yaml

--- hunter_pkg/stats.py
+++ hunter_pkg/stats.py
@@ -1,5 +1,5 @@
 import functools
-import json
+import yaml
 
 from hunter_pkg import flogging
 from hunter_pkg import log_level
@@ -11,11 +11,11 @@ class Stats():
     _map = None
 
     @classmethod
-    def map(cls, file_name="hunter_pkg/config/stats.json"):
+    def map(cls, file_name="hunter_pkg/config/stats.yaml"):
         if(cls._map is None):
             with open(file_name) as file:
                 flog.debug("opened a file")
-                cls._map = json.load(file)
+                cls._map = yaml.safe_load(file)
                 return cls._map
 
         return cls._map
